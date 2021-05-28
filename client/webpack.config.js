54a626537e4566161849bacd886df7e8a4711ecd
Lara Hayward <haywardlbl@gmail.com>
2021-05-28 12:03:22 +1200

    sass set-up

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,7 +12,19 @@ module.exports = {
       test: /\.jsx?$/,
       loader: 'babel-loader',
       exclude: /node_modules/
-    }]
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+    },
+  ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
